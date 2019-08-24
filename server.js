a53fb017845d729e6772ef6c6b7c936a931c15e7
Scott Finlay <xisifer@gmail.com>
2019-08-23 20:40:52 -0500

    Updated a connection path.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // =========================================================
 const express = require("express");
 const path = require("path");
-const friends = require("app/data/friends")
+const friends = require("./app/data/friends")
 
 
 // For serving of static CSS
