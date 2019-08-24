abe8670180f073afdf83460b2c5f8bcb47b7d4b4
Scott Finlay <xisifer@gmail.com>
2019-08-24 16:41:14 -0500

    Tweaking HTML, adding flavor text for the friends.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,50 +2,50 @@ var friends = [
 	{
 		name: "Jar Jar Binks",
 		photo: "https://pbs.twimg.com/profile_images/699357286167416832/LxslBk87.jpg",
-        scores: [5, 1, 5, 5, 3, 
-            1, 2, 1, 3, 1]
+        scores: [5, 1, 5, 5, 3, 1, 2, 1, 3, 1],
+        quote: "You sympathize with "
 	},
 	{
 		name: "Luke Skywalker",
 		photo: "https://pbs.twimg.com/profile_images/821436707790843908/1MweYEUL_400x400.jpg",
-        scores: [5, 1, 3, 1, 5, 
-            1, 1, 1, 1, 1]
+        scores: [5, 1, 3, 1, 5, 1, 1, 1, 1, 1],
+        quote: "You sympathize with "
 	},
 	{
 		name: "Anakin Skywalker",
 		photo: "http://pm1.narvii.com/6025/cfef367bbc8fa89e2921efca288f53436c111e8e_00.jpg",
-        scores: [5, 5, 3, 5, 5, 
-            5, 5, 5, 5, 5]
+        scores: [5, 5, 3, 5, 5, 5, 5, 5, 5, 5],
+        quote: "You sympathize with Anakin Skywalker, the visionary former Jedi who realized the glory of the Empire after being betrayed by the Jedi scum."
 	},
 	{
 		name: "Sheev Palpatine",
 		photo: "https://pbs.twimg.com/profile_images/1096658348915740672/TvUZhaLX_400x400.png",
-        scores: [5, 5, 4, 5, 5, 
-            5, 5, 5, 5, 5]
+        scores: [5, 5, 4, 5, 5, 5, 5, 5, 5, 5],
+        quote: "You sympathize with our glorious Emperor Palpatine. Congratulations!"
 	},
 	{
 		name: "Leia Organa Skywalker",
 		photo: "https://pbs.twimg.com/profile_images/948342326686076929/lJDuXU2w_400x400.jpg",
-        scores: [5, 1, 3, 1, 2, 
-            1, 1, 1, 1, 1]
+        scores: [5, 1, 3, 1, 2, 1, 1, 1, 1, 1],
+        quote: "You sympathize with "
     },
     {
 		name: "GRUMPY CAT",
 		photo: "https://pbs.twimg.com/profile_images/1080545769034108928/CEzHCTpI_400x400.jpg",
-        scores: [1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1]
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        quote: "You sympathize with "
     },
     {
 		name: "Han Solo",
 		photo: "https://cdn.shopify.com/s/files/1/1368/0279/products/Han_Solo_1024x1024.jpg",
-        scores: [5, 1, 3, 1, 1, 
-            1, 1, 1, 1, 1]
+        scores: [5, 1, 3, 1, 1, 1, 1, 1, 1, 1],
+        quote: "You sympathize with "
     },
     {
 		name: "Obi-Wan Kenobi",
 		photo: "https://pbs.twimg.com/profile_images/1052718076859363328/TGSm9oWU_400x400.jpg",
-        scores: [5, 1, 4, 1, 1, 
-            1, 1, 1, 1, 1]
+        scores: [5, 1, 4, 1, 1, 1, 1, 1, 1, 1],
+        quote: "You sympathize with "
 	}
 ];
 
