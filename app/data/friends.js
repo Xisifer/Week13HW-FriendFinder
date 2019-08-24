a2edd71b3cba5cc8d4a602241314941eb9b0f50f
Scott Finlay <xisifer@gmail.com>
2019-08-24 14:51:58 -0500

    Customized the survey and friend data.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,51 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
-		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+		name: "Jar Jar Binks",
+		photo: "https://pbs.twimg.com/profile_images/699357286167416832/LxslBk87.jpg",
+        scores: [5, 1, 5, 5, 3, 
+            1, 2, 1, 3, 1]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
-		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+		name: "Luke Skywalker",
+		photo: "https://pbs.twimg.com/profile_images/821436707790843908/1MweYEUL_400x400.jpg",
+        scores: [5, 1, 3, 1, 5, 
+            1, 1, 1, 1, 1]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+		name: "Anakin Skywalker",
+		photo: "http://pm1.narvii.com/6025/cfef367bbc8fa89e2921efca288f53436c111e8e_00.jpg",
+        scores: [5, 5, 3, 5, 5, 
+            5, 5, 5, 5, 5]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+		name: "Sheev Palpatine",
+		photo: "https://pbs.twimg.com/profile_images/1096658348915740672/TvUZhaLX_400x400.png",
+        scores: [5, 5, 4, 5, 5, 
+            5, 5, 5, 5, 5]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
-		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+		name: "Leia Organa Skywalker",
+		photo: "https://pbs.twimg.com/profile_images/948342326686076929/lJDuXU2w_400x400.jpg",
+        scores: [5, 1, 3, 1, 2, 
+            1, 1, 1, 1, 1]
     },
     {
 		name: "GRUMPY CAT",
 		photo: "https://pbs.twimg.com/profile_images/1080545769034108928/CEzHCTpI_400x400.jpg",
-		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [1, 1, 1, 1, 1, 
+            1, 1, 1, 1, 1]
+    },
+    {
+		name: "Han Solo",
+		photo: "https://cdn.shopify.com/s/files/1/1368/0279/products/Han_Solo_1024x1024.jpg",
+        scores: [5, 1, 3, 1, 1, 
+            1, 1, 1, 1, 1]
+    },
+    {
+		name: "Obi-Wan Kenobi",
+		photo: "https://pbs.twimg.com/profile_images/1052718076859363328/TGSm9oWU_400x400.jpg",
+        scores: [5, 1, 4, 1, 1, 
+            1, 1, 1, 1, 1]
 	}
 ];
 
