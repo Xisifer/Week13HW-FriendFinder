65b16eeda31ab1e4a276e360413ec8cd58719151
Scott Finlay <xisifer@gmail.com>
2019-08-24 21:15:42 -0500

    Finished the flavor text on the friends database.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,49 +3,49 @@ var friends = [
 		name: "Jar Jar Binks",
 		photo: "https://pbs.twimg.com/profile_images/699357286167416832/LxslBk87.jpg",
         scores: [5, 1, 5, 5, 3, 1, 2, 1, 3, 1],
-        quote: "You sympathize with "
+        quote: "You sympathize with Jar Jar Binks, an oafish buffoon, not to mention a subhuman alien. Perhaps we should be questioning your Imperial loyalties?"
 	},
 	{
 		name: "Luke Skywalker",
 		photo: "https://pbs.twimg.com/profile_images/821436707790843908/1MweYEUL_400x400.jpg",
         scores: [5, 1, 3, 1, 5, 1, 1, 1, 1, 1],
-        quote: "You sympathize with "
+        quote: "You sympathize with Luke Shywalker, known terrorist leader and Rebel puppet. For shame!"
 	},
 	{
 		name: "Anakin Skywalker",
 		photo: "http://pm1.narvii.com/6025/cfef367bbc8fa89e2921efca288f53436c111e8e_00.jpg",
         scores: [5, 5, 3, 5, 5, 5, 5, 5, 5, 5],
-        quote: "You sympathize with Anakin Skywalker, the visionary former Jedi who realized the glory of the Empire after being betrayed by the Jedi scum."
+        quote: "You identify with Anakin Skywalker, the visionary former Jedi who realized the glory of the Empire after being betrayed by the Jedi scum."
 	},
 	{
 		name: "Sheev Palpatine",
 		photo: "https://pbs.twimg.com/profile_images/1096658348915740672/TvUZhaLX_400x400.png",
         scores: [5, 5, 4, 5, 5, 5, 5, 5, 5, 5],
-        quote: "You sympathize with our glorious Emperor Palpatine. Congratulations!"
+        quote: "You identify with our glorious Emperor Palpatine. Congratulations!"
 	},
 	{
-		name: "Leia Organa Skywalker",
+		name: "Leia Organa",
 		photo: "https://pbs.twimg.com/profile_images/948342326686076929/lJDuXU2w_400x400.jpg",
         scores: [5, 1, 3, 1, 2, 1, 1, 1, 1, 1],
-        quote: "You sympathize with "
+        quote: "You sympathize with Leia Organa, a known Rebel firebrand and sympathizer. Please attend your next Imperial re-education seminar."
     },
     {
 		name: "GRUMPY CAT",
 		photo: "https://pbs.twimg.com/profile_images/1080545769034108928/CEzHCTpI_400x400.jpg",
         scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        quote: "You sympathize with "
+        quote: "You sympathize with GRUMPY CAT, who HATES EVERYTHING. ALWAYS."
     },
     {
 		name: "Han Solo",
 		photo: "https://cdn.shopify.com/s/files/1/1368/0279/products/Han_Solo_1024x1024.jpg",
         scores: [5, 1, 3, 1, 1, 1, 1, 1, 1, 1],
-        quote: "You sympathize with "
+        quote: "You sympathize with Han Solo, a no-name fringe pilot flying a hunk of junk nobody's ever heard of."
     },
     {
 		name: "Obi-Wan Kenobi",
 		photo: "https://pbs.twimg.com/profile_images/1052718076859363328/TGSm9oWU_400x400.jpg",
         scores: [5, 1, 4, 1, 1, 1, 1, 1, 1, 1],
-        quote: "You sympathize with "
+        quote: "You sympathize with Obi-Wan Kenobi, an Old Republic traitor linked to the failed Jedi uprisings of the Clone Wars."
 	}
 ];
 
